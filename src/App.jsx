058107acbe7a4f80fa1c56b1777eb15cfeaa95b4
Esteban Vanegas <juanestebanvanegasvela@gmail.com>
2024-01-12 12:01:18 -0500

    Se instala libreria Chakra UI correctamente

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react'
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header.jsx';
